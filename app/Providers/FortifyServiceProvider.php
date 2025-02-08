cd1ce3fa124479d8fa192b8eed582aaeceecca23
dpunkz09 <dpunkz09@gmail.com>
2025-02-08 20:10:16 +0800

    Update FortifyServiceProvider.php

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -2,21 +2,24 @@
 
 namespace App\Providers;
 
-use App\Actions\Fortify\SendPasswordResetLink;
-use App\Actions\Fortify\ResetUserPassword;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Fortify;
 
 class FortifyServiceProvider extends ServiceProvider
 {
+    public function register()
+    {
+        //
+    }
+
     public function boot()
     {
-        // Use the custom action to send password reset links
-        Fortify::requestPasswordResetLinkUsing(SendPasswordResetLink::class);
+        Fortify::resetPasswordView(function () {
+            return view('auth.reset-password');
+        });
 
-        // Reset User Passwords
-        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
-
-        // Other customizations...
+        Fortify::requestPasswordResetLinkView(function () {
+            return view('auth.forgot-password');
+        });
     }
 }
